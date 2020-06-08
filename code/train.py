5f4fd5251f7ad3bac04b45c8871587c2c9ec2d73
Fyaushev <yaushev@phystech.edu>
2020-06-08 19:20:04 +0300

    Added logging to TensorBoatd

--- code/train.py
+++ code/train.py
@@ -1,10 +1,13 @@
 import argparse
 import json
+import torch
 
 from data_loader import get_data_loader
 from models import get_models, get_params, predict
 from loss import get_losses, compute_losses
 from optimizer import get_optimizer
+
+from torch.utils.tensorboard import SummaryWriter
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -22,29 +25,45 @@ def read_params(fid):
 def main(args):
     params = read_params(args.params_path)
 
+    writer = SummaryWriter('runs/' + params['name'])
+
     train_loader = get_data_loader(params['data_type'], train=True, batch_size=params['batch_size'])
     valid_loader = get_data_loader(params['data_type'], train=False, batch_size=params['batch_size'])
 
     models = get_models(params['models'])
     losses = get_losses(params['losses'])
 
+    dataiter = iter(train_loader)
+    inputs, _ = dataiter.next()
+
+    writer.add_graph(models, inputs)
+
     model_params = get_params(models)
     optimizer = get_optimizer(params['optimizer'], model_params)
 
-    for step, (x, y) in enumerate(train_loader):
-        loss_values = compute_losses(losses, models, x, y)
-        optimizer.zero_grad()
+    running_loss = 0.0
+    for epoch in range(params['num_epochs']):
+        for step, (x, y) in enumerate(train_loader):
+            loss_values = compute_losses(losses, models, x, y)
+            optimizer.zero_grad()
 
-        loss_values['total_loss'].backward()
-        optimizer.step()
+            loss_values['total_loss'].backward()
+            optimizer.step()
 
-        if step % params['summary']['train_frequency'] == 0:
-            pass
+            running_loss += loss_values['total_loss'].item()
+            if step % params['summary']['train_frequency'] == 0:
+                pass
 
-        if step % params['summary']['save_frequency'] == 0:
-            pass
-
-        break
+            if step % params['summary']['save_frequency'] == 0:
+                writer.add_scalar('training loss',
+                                  running_loss / params['summary']['save_frequency'],
+                                  epoch * len(train_loader) + step)
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': models.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss_values,
+                }, 'model_backup/' + params['name'] + '/' + str(epoch))
 
 
 if __name__ == '__main__':
